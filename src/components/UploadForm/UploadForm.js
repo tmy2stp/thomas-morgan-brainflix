44758aec89af316439edaa62e3f864fc5eba6609
Thomas Morgan <Tommorgan86@gmail.com>
2022-08-24 00:47:05 -0400

    upload page now resembles mockups at tablet and desktop dimensions, hid the divider on higher resolutions and used flex layout to arrange the items

--- src/components/UploadForm/UploadForm.js
+++ src/components/UploadForm/UploadForm.js
@@ -8,6 +8,7 @@ function Upload() {
             <div className="upload__margin"></div>
             <div className="upload">
                 <h3 className="upload__title">Upload Video</h3>
+                <div className="upload__margin-tablet"></div>
                 <span className="upload__label">Video Thumbnail</span>
                 <img className="upload__thumbnail" src={videoThumbnail} alt="" />
                 <form className="upload__form">
@@ -16,6 +17,7 @@ function Upload() {
                     <label for="form__video-description" className="upload__label">Add a video description</label>
                     <textarea id="form__video-description" className="upload__text-input-box-large" placeholder="Add a description to your video"></textarea>
                 </form>
+                <div className="upload__margin-tablet"></div>
                 <button className="upload__publish-btn">
                     <img src={publishBtn} alt="" className="upload__btn-icon" />PUBLISH
                 </button>
