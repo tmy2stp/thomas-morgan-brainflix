febae215a20674e682e5c8cce92381c0d6462922
Thomas Morgan <Tommorgan86@gmail.com>
2022-08-24 21:10:12 -0400

    Added and styled links to header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,13 @@ import logo from '../../assets/images/BrainFlix-logo.svg'
 import avatar from '../../assets/images/Mohan-muruge.jpg'
 import uploadBtn from '../../assets/icons/upload.svg'
 import search from '../../assets/icons/search.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
     return (
         <header className="header">
-                <img src={logo} alt="" className="header__logo" />
+                <Link to="/" className="header__logo"><img src={logo} alt="" /></Link>
                 <form className='header__form'>
                     <button type="submit" className="header__search-button">
                         <img src={search} alt="" />
@@ -16,9 +17,11 @@ const Header = () => {
                     <input type="text" id="searchText" name="searchText" placeholder='Search' className="header__search-box" />
                 </form>
                 <img src={avatar} alt="" className="header__avatar" />
-                <button className="header__upload-btn">
-                    <img src={uploadBtn} alt="" className="header__upload-btn-icon" />UPLOAD
-                </button>
+                <Link to="/upload" className="header__upload-link">
+                    <button className="header__upload-btn">
+                        <img src={uploadBtn} alt="" className="header__upload-btn-icon" />UPLOAD
+                    </button>
+                </Link>
         </header>
     )
 
