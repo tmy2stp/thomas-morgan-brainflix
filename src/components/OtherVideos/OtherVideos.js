552541c43cd2704f0de48e83c87480fec1438370
Thomas Morgan <Tommorgan86@gmail.com>
2022-08-25 23:08:47 -0400

    refactored click event to use link instead, added default margins for body

--- src/components/OtherVideos/OtherVideos.js
+++ src/components/OtherVideos/OtherVideos.js
@@ -1,22 +1,23 @@
 import './OtherVideos.scss';
+import { Link } from 'react-router-dom';
 
 function OtherVideos(props) {
     let idToExclude = props.otherVideoData[0].id;
-    let spanArray = props.otherVideoData[1].filter(e => e.id !== idToExclude).map((element,i) => 
-        <div key= {element.id} className='otherVideos__container'>
-            <div className='otherVideos__thumbnail-container'>
-                <img className='otherVideos__thumbnail' src={element.image} alt={element.title} />
-            </div>
-            <div className='otherVideos__info-container'>
-                
-        <div className='otherVideos__info-title'>{element.title}</div>
-        <div className='otherVideos__info'>{element.channel}</div>
-            </div>
-        </div>);
+    let spanArray = props.otherVideoData[1].filter(e => e.id !== idToExclude).map((element, i) =>
+        <Link to={"/" + element.id} className='otherVideos__container'>
+                <div className='otherVideos__thumbnail-container'>
+                    <img className='otherVideos__thumbnail' src={element.image} alt={element.title} />
+                </div>
+                <div className='otherVideos__info-container'>
+
+                    <div className='otherVideos__info-title'>{element.title}</div>
+                    <div className='otherVideos__info'>{element.channel}</div>
+                </div>
+            </Link>);
     return (
         <section className='otherVideos'>
             <h3 className='otherVideos__title'>NEXT VIDEOS</h3>
-        {spanArray}
+            {spanArray}
         </section>
     );
 }
